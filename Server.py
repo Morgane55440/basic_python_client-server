9aa1134674d2c7678ea538c8137f15fd491292a0
Morgane Baizeau <morgane.baizeau@epita.fr>
2022-10-05 18:56:45 +0200

    coded the server. not tested, need client

--- Server.py
+++ Server.py
@@ -6,23 +6,51 @@ import query_struct
 
 import socket
 import asyncio
+import pickle
 
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 55555
-MAX_DATA_SIZE = 256
+MAX_DATA_SIZE = 1024
 CLIENT_NB = 1
 
 
-query_struct.Query(query_struct.Query_type.COMPUTE)
 
-"""
-server = socket.socket()         
-server.bind((HOST, PORT))        
+async def async_exec(query : query_struct.Query) -> None:
+    if (query.type == query_struct.Query_type.COMPUTE_FIBO):
+        res = fibo_c(query.value) # Where there is potentially a long exectution time
+        print(query.value)
+        query.value = res
+        query.status = query_struct.Query_Status.DONE
+
+
+
+
+async def run_server(client_socket):
+    task_list = []
+    result_list = []
+    running = True
+    while running:
+        query = pickle.load(client_socket.recv(MAX_DATA_SIZE))
+        if query.type == query_struct.Query_type.COMPUTE_FIBO:
+            query.status = query_struct.Query_Status.SCHEDULED
+            task_list.append(async_exec(query))
+            result_list.append(query)
+        if query.type == query_struct.Query_type.LIST:
+            for res in result_list:
+                client_socket.send(pickle.dump(res))
+            client_socket.send(pickle.dump(query_struct.Query(query_struct.Query_type.LIST_END)))
+    for task in task_listl:
+        await task.status
+
+
+
+server_socket = socket.socket()         
+server_socket.bind((HOST, PORT))        
 
 
 while True:
-    servers.listen(CLIENT_NB)
-    client, addr = server.accept()
+    server_socket.listen(CLIENT_NB)
+    client, addr = server_socket.accept()
+    asyncio.run(run_server(client))
         
-"""
--- query_struct.py
+++ query_struct.py
@@ -2,11 +2,11 @@ from enum import Enum
 
 
 class Query_type(Enum):
-    COMPUTE = 0
+    COMPUTE_FIBO = 0
     LIST = 1
 
 class Query_Status(Enum):
-    TO_BE_SENT = 0
+    TO_BE_SCHEDULED = 0
     SCHEDULED = 1
     DONE = 2
 
@@ -14,5 +14,5 @@ class Query:
     def __init__(self, type : Query_type, value = None ) -> None:
         self.type = type
         self.value = value
-        self.status = Query_Status.TO_BE_SENT
+        self.status = Query_Status.TO_BE_SCHEDULED
         self.result = None
